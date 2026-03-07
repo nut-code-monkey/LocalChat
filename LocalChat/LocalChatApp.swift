b839333a645c4f2f83af49c18033352b4a059ab2
Maksym Lunin <nut.code.monkey@gmail.com>
2026-03-07 18:09:42 +0200

    Initial Commit

---
+++ LocalChat/LocalChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatApp.swift
+//  LocalChat
+//
+//  Created by Максим Лунін on 07.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LocalChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalChatTests/LocalChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  LocalChatTests.swift
+//  LocalChatTests
+//
+//  Created by Максим Лунін on 07.03.2026.
+//
+
+import Testing
+
+struct LocalChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
